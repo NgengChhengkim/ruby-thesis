d27a356482f4e6346f0c1f3ceec250a59415d2ef
ngengchhengkim <chhengkimngeng@gmail.com>
2016-10-14 23:17:25 +0700

    Fix N+1 query

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @activities = Activity.all
+    @activities = Activity.all.preload( user: :user_profile, trackable: [job_post: :job_type])
     @job_types = JobType.all
     @post = Post.new
     @post.build_job_post
